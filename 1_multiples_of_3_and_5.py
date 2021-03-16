33b9d52329ce4bc21e2f1b1795a2c86f62fddbbc
Dimitrij Kolničenko <dimitij.kol@protonmail.com>
2021-03-16 01:03:05 +0100

    problem 1 refactored

--- 1_multiples_of_3_and_5.py
+++ 1_multiples_of_3_and_5.py
@@ -4,5 +4,11 @@ The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
-print(sum(num for num in range(1, 1000)
-          if num % 3 == 0 or num % 5 == 0))
+def sum_of_multiples(rng=(1, 1000), multiples=(3, 5)):
+    final_sum = 0
+    for num in range(*rng):
+        for m in multiples:
+            if num % m == 0:
+                final_sum += num
+                break
+    return final_sum
