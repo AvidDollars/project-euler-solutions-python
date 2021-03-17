332ebe5eed78828377c9acb48209396c0d162956
Dimitrij Kolničenko <dimitij.kol@protonmail.com>
2021-03-17 23:16:40 +0100

    time class modified so that arguments can be provided to @register()

--- timer.py
+++ timer.py
@@ -28,32 +28,40 @@ class timer:
     timer aimed for comparing runtime of different functions
 
     usage:
-        1) register a functions you want to time with @register
+        1) register a function you want to time with @register()
+            - if decorated function takes arguments, they must be provided as parameters
+              to @register()
+            - if decorated function has default values, they can be overwritten by providing
+              arguments to @register()
         2) after all functions are registered, perform timing with 'timer.run()'
     """
 
-    registry = []
+    registry = {}
 
     @classmethod
-    def register(cls, fn):
-        cls.registry.append(fn)
-        return
+    def register(cls, *arg):
+        def inner(fn):
+            cls.registry[fn] = arg or fn.__defaults__ or ()
+            return fn
+        return inner
 
     @classmethod
     def run(cls, repeats=5_000):
-        for fn in cls.registry:
-            cls._timer(fn, repeats)
+        for fn, params in cls.registry.items():
+            cls._timer(fn, repeats, *params)
 
     @staticmethod
-    def _timer(fn, repeats):
-        @_wraps(fn)
-        def inner(*a, **kw):
+    def _timer(fn, repeats, *arg):
+        def inner(arg):
             start = _perf_counter()
             for n in range(repeats - 1):
-                fn(*a, **kw)
-            result = fn(*a, **kw)
+                fn(*arg)
+            result = fn(*arg)
             elapsed_time = _perf_counter() - start
-            print(f"elapsed time of '{fn.__name__}': {round(elapsed_time, 3)}s (repeats={repeats})")
-            return result
-        return inner()
+            print(f"'{fn.__qualname__}':")
+            print(f"\telapsed time: {elapsed_time:.3}s", end=", ")
+            print("repeats: ", repeats, end=", ")
+            print("result: ", result)
+            print()
+        return inner(arg)
 
