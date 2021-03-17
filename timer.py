b011ffc2e8067c8b260e53f1bb8a96a7bbdf2c45
Dimitrij Kolničenko <dimitij.kol@protonmail.com>
2021-03-17 21:08:06 +0100

    added timer class for simpler timing between functions executing same task

--- timer.py
+++ timer.py
@@ -21,3 +21,39 @@ def parameterized_timer(repeats=100_000):
             return result
         return inner
     return timer_decorator
+
+
+class timer:
+    """
+    timer aimed for comparing runtime of different functions
+
+    usage:
+        1) register a functions you want to time with @register
+        2) after all functions are registered, perform timing with 'timer.run()'
+    """
+
+    registry = []
+
+    @classmethod
+    def register(cls, fn):
+        cls.registry.append(fn)
+        return
+
+    @classmethod
+    def run(cls, repeats=5_000):
+        for fn in cls.registry:
+            cls._timer(fn, repeats)
+
+    @staticmethod
+    def _timer(fn, repeats):
+        @_wraps(fn)
+        def inner(*a, **kw):
+            start = _perf_counter()
+            for n in range(repeats - 1):
+                fn(*a, **kw)
+            result = fn(*a, **kw)
+            elapsed_time = _perf_counter() - start
+            print(f"elapsed time of '{fn.__name__}': {round(elapsed_time, 3)}s (repeats={repeats})")
+            return result
+        return inner()
+
