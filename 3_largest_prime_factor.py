913142a951c2ffdc3a63d9b78a089ccbf6c98bc1
Dimitrij Kolničenko <dimitij.kol@protonmail.com>
2021-05-01 16:45:56 +0200

    added function for generating prime numbers

--- 3_largest_prime_factor.py
+++ 3_largest_prime_factor.py
@@ -2,3 +2,34 @@
 The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 """
+
+from timer import timer
+from math import floor, sqrt
+import itertools
+
+#TODO: implement proper data structure for keeping prime factors in sorted manner
+
+
+def generate_primes():
+    # ↓↓↓ yields predefined prime numbers
+    predefined = (2, 3, 5, 7)
+    yield from predefined
+
+    # we are interested only in yielding numbers ending with digit 1, 3, 7 or 9
+    # ↓↓↓ if incremented from number 11, it will not yield any number divisible by 5
+    increment = itertools.cycle((2, 4, 2, 2))
+    num = 11
+
+    # ↓↓↓ yields calculated prime numbers
+    while True:
+        for d in range(2, floor(sqrt(num)) + 1):
+            if num % d == 0:
+                num += next(increment)
+                break
+        else:
+            yield num
+            num += next(increment)
+
+
+def largest_prime_factor(num: int) -> int:
+    pass
